1ff092c49c632e455b3c091e89d5c4409c43fd81
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2026-05-09 10:52:50 -0300

    feat(config): adiciona campo cgi_extensions em RouteConfig

--- src/includes/RouteConfig.hpp
+++ src/includes/RouteConfig.hpp
@@ -11,18 +11,20 @@
 /* ************************************************************************** */
 
 #pragma once
+#include <map>
 #include <string>
 #include <vector>
 
 struct RouteConfig {
-    std::string              path;
-    std::vector<std::string> methods;
-    std::string              index;
-    bool                     autoindex;
-    int                      redirect_code;
-    std::string              redirect_url;
-    std::string              upload_store;
-    std::string              root;
+    std::string                        path;
+    std::vector<std::string>           methods;
+    std::string                        index;
+    bool                               autoindex;
+    int                                redirect_code;
+    std::string                        redirect_url;
+    std::string                        upload_store;
+    std::string                        root;
+    std::map<std::string, std::string> cgi_extensions;
 
     RouteConfig() : autoindex(false), redirect_code(0) {}
 };
